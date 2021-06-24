2d130b0e4131f30d28273e0505bfb3a9f674509e
Pikachu <jouxiong15@gmail.com>
2021-06-23 23:02:48 -0500

    added heading

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,7 +3,28 @@ import React from "react";
 function Heading() {
   return (
     <div>
-        
+      <header>
+        <div id="top">
+          <h1>Jou Xiong's Online Portfolio</h1>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <a href="#about">About Me</a>
+            </li>
+            <li>
+              <a href="#work">My Work</a>
+            </li>
+            <li>
+              <a href="#contact">Contact Me</a>
+            </li>
+          </ul>
+        </div>
+      </header>
+
+      <div class="coverPhoto" alt="blue background">
+        <h2>My Journey So Far</h2>
+      </div>
     </div>
   );
 }
