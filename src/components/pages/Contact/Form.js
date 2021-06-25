dc9341c6b81ec23097dd8229b343f01ab9e38b90
Pikachu <jouxiong15@gmail.com>
2021-06-24 22:40:38 -0500

    got formspree to work

--- src/components/pages/Contact/Form.js
+++ src/components/pages/Contact/Form.js
@@ -1,45 +1,40 @@
-import React from "react";
-import emailjs from "emailjs-com";
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
-export default function Form() {
-
-    function sendEmail(e) {
-        e.preventDefault();
-        emailjs.sendForm('gmail', 'template_ntxe6g9', e.target, 'user_urGX8ANaqHgudfBxPHZF9')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-        e.target.reset();
-    }
-
-    return (
-        <div>
-            <div className="container">
-                <form onSubmit={sendEmail}>
-                    <div className="row form-group mx-auto">
-                        <div className="col-8 form-group mx-auto">
-                            <input type="text" className="form-control" placeholder="Name" name="name" />
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="email" className="form-control" placeholder="Email Address" name="email" />
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="text" className="form-control" placeholder="Subject" name="subject" />
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your Message" name="message"></textarea>
-                        </div>
-                        <div className="col-8 pt-3 mx-auto">
-                            <input type="submit" className="btn btn-info" value="Send Message"></input>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
-
+function Form() {
+  const [state, handleSubmit] = useForm("moqylqpk");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
+  return (
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" action="https://formspree.io/f/moqylqpk">
+        Submit
+      </button>
+    </form>
+  );
 }
 
-
+export default Form;
