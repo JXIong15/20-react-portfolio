f0ff7d34b2e81f23f63f671c0b04b4c3113f41e8
Pikachu <jouxiong15@gmail.com>
2021-06-26 19:08:59 -0500

    changed links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,17 +5,17 @@ function NavBar() {
   return (
     <ul className="nav navbar">
       <li className="nav-item">
-        <Link to="/">
+        <Link to="/20-react-portfolio/">
           About Me
         </Link>
       </li>
       <li className="nav-item">
-        <Link to="/work">
+        <Link to="/20-react-portfolio/work">
           My Work
         </Link>
       </li>
       <li className="nav-item">
-        <Link to="/contact">
+        <Link to="/20-react-portfolio/contact">
           Contact
         </Link>
       </li>
